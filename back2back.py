453c9b8476e68a98cf226a8659edf0b9b4c2bfe1
tibigg <tg4018@ic.ac.uk>
2022-02-24 23:45:14 +0000

    Augmenting pcl data with back2back translations

--- back2back.py
+++ back2back.py
@@ -1,5 +1,7 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+from transformers import AutoTokenizer, \
+    AutoModelForSeq2SeqLM, pipeline
 from enum import Enum
+import os
 
 
 class Language(Enum):
@@ -7,9 +9,21 @@ class Language(Enum):
 
 
 def translation_pipeline_from_model(model_name):
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-    enpt_pipeline = pipeline('text2text-generation', model=model, tokenizer=tokenizer)
+    if os.path.isdir('tokens/' + model_name):
+        tokenizer = AutoTokenizer.from_pretrained('tokens/' + model_name)
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        tokenizer.save_pretrained('tokens/' + model_name)
+
+    if os.path.isdir('models/' + model_name):
+        model = AutoModelForSeq2SeqLM.from_pretrained('models/' + model_name)
+    else:
+        model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+        model.save_pretrained('models/' + model_name)
+
+    enpt_pipeline = pipeline('text2text-generation',
+                             model=model,
+                             tokenizer=tokenizer)
     return enpt_pipeline
 
 
@@ -18,13 +32,16 @@ class Back2BackTranslator:
         self._translators_to = dict()
         self._translators_from = dict()
 
+        en_pt_translator_model_name = "unicamp-dl/translation-en-pt-t5"
+        pt_en_translator_model_name = "unicamp-dl/translation-pt-en-t5"
         self._translators_to[Language.PT] = \
-            translation_pipeline_from_model("unicamp-dl/translation-en-pt-t5")
+            translation_pipeline_from_model(en_pt_translator_model_name)
         self._translators_from[Language.PT] = \
-            translation_pipeline_from_model("unicamp-dl/translation-pt-en-t5")
+            translation_pipeline_from_model(pt_en_translator_model_name)
 
     def translate_from(self, language: Language, text_in_language):
-        return self._translators_from[language](text_in_language)[0]['generated_text']
+        return self._translators_from[language](text_in_language)[0][
+            'generated_text']
 
     def translate_to(self, language: Language, text_en):
         return self._translators_to[language](text_en)[0]['generated_text']
